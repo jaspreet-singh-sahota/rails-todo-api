f9ab75d43b6f4647db352662d65c1d00cee8a04f
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-07 00:50:41 +0530

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :todos do
+    resources :items
+  end
 end

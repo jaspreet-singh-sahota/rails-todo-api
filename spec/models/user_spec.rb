b756fea3da67453744e0a05fc1e3466a97fc4cf4
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-08 02:38:40 +0530

    Add tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
+# spec/models/user_spec.rb
 require 'rails_helper'
 
+# Test suite for User model
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure User model has a 1:m relationship with the Todo model
+  it { should have_many(:todos) }
+  # Validation tests
+  # ensure name, email and password_digest are present before save
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
 end
